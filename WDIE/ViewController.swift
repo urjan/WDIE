ad915ff6146cd864ed9b0a4807caa96c854717ff
urjan <urjan@seas.upenn.edu>
2017-06-24 18:38:19 -0400

    Just getting started: segue works from main screen to display screen

--- WDIE/ViewController.swift
+++ WDIE/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WhatButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func WhatButtonAction(_ sender: Any) {
+        performSegue(withIdentifier: "displaySegue", sender: self)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
